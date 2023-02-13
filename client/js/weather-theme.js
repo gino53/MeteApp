da946ccd7725d285e780aac9d1787d7b88d9999d
gino53 <castagnagino5@gmail.com>
2023-02-13 03:18:19 +0100

    weather app 1

---
+++ client/js/weather-theme.js
@@ -0,0 +1,27 @@
+const toggleBtn = document.querySelector('.theme__btn');
+const body = document.querySelector('body');
+let lightMode = localStorage.getItem('light-mode');
+
+const enableLightMode = () => {
+    body.classList.add('light');
+    localStorage.setItem('light-mode', 'enabled');
+};
+
+const disableLightMode = () => {
+    body.classList.remove('light');
+    localStorage.setItem('light-mode', 'disabled');
+
+};
+
+if (lightMode === 'enabled') {
+    enableLightMode();
+}
+
+toggleBtn.addEventListener('click', (e) => {
+    lightMode = localStorage.getItem('light-mode');
+    if (lightMode === 'disabled') {
+        enableLightMode();
+    } else {
+        disableLightMode();
+    }
+});
