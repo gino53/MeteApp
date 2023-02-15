3a1e789cd490a6ec1abcfedc09c1fbf193480043
gino53 <castagnagino5@gmail.com>
2023-02-15 14:53:11 +0100

    min, max temp /class/ js min

--- client/js/main.js
+++ client/js/main.js
@@ -19,7 +19,7 @@ startTime();
 function startDate() {
     var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth() + 1; // janvier est 0
+    var mm = today.getMonth() + 1;
     var yyyy = today.getFullYear();
 
     if (dd < 10) {
