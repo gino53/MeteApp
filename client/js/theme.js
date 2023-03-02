58e117dbdd67e11a82f7eea17791110a6c517f46
gino53 <castagnagino5@gmail.com>
2023-03-02 16:16:29 +0100

    final version

--- client/js/theme.js
+++ client/js/theme.js
@@ -1,14 +1,20 @@
-const toggleBtn = document.getElementById('darkmode-toggle');
+const toggleBtn = document.getElementById('darkmode_toggle');
 const body = document.querySelector('body');
+const github = document.getElementById('github_icon');
+const linkedin = document.getElementById('linkedin_icon');
 let lightMode = localStorage.getItem('light-mode');
 
 const enableLightMode = () => {
     body.classList.add('light');
+    linkedin.src = ('/linkedin-black.png');
+    github.src = ('/github-black.png');
     localStorage.setItem('light-mode', 'enabled');
 };
 
 const disableLightMode = () => {
     body.classList.remove('light');
+    linkedin.src = ('/linkedin.png');
+    github.src = ('/github.png');
     localStorage.setItem('light-mode', 'disabled');
 };
 
